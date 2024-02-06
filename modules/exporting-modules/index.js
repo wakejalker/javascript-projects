006226c0f8b8a0d8b185791b08f73a1ad85fbef7
Jake Walker <jake@Jakes-MBP.attlocal.net>
2024-02-05 20:16:44 -0600

    module excercises

--- modules/exporting-modules/index.js
+++ modules/exporting-modules/index.js
@@ -1,1 +1,19 @@
 // Import the modules exported from practiceExports.js below:
+const palindromeCheck = require('./practiceExports.js');
+
+// console.log(typeof palindromeCheck);
+// console.log(palindromeCheck('that'));
+// console.log(palindromeCheck('radar'));
+
+const practice = require('./practiceExports.js');
+let arr = ['Hello', 'World', 123, 987, 'LC101'];
+
+// console.log(typeof practice);
+// console.log(practice);
+
+console.log(practice.isPalindrome('mom'));
+console.log(practice.evenOrOdd(9));
+
+for (i = 0; i < 3; i++){
+    console.log(practice.randomArrayElement(arr));
+}
