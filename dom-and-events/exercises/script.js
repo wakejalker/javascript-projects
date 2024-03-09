8c06f73e119499e383704c0976d2e5eef34f6b99
jake <iamjakewalker@gmail.com>
2024-03-09 14:13:51 -0600

    DOM and Events Exercises

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -4,7 +4,27 @@ function init () {
     const paragraph = document.getElementById("statusReport");
 
     // Put your code for the exercises here.
-    
+    button.addEventListener("click", function() {
+        paragraph.innerHTML = "Houston, we have liftoff!";
+    });
+
+    missionAbort.addEventListener("mouseover", function() {
+        missionAbort.style.backgroundColor = "red";
+    });
+
+    missionAbort.addEventListener("mouseout", function(){
+        missionAbort.style.backgroundColor = "";
+    });
+
+    missionAbort.addEventListener("click", function () {
+        const confirmAbort = confirm("Are you sure you  want to abort the mission?");
+
+        if (confirmAbort) {
+            paragraph.innerHTML = "Mission aborted! Space shuttle returning home."
+        }
+
+    });
+
 }
 
 window.addEventListener("load", init);
