daf49a4fe65f7cd9948ed4ec23cb9d4122006546
jake <32205542+wakejalker@users.noreply.github.com>
2024-03-21 22:46:48 -0500

    Fetch Studio

--- fetch/studio/script.js
+++ fetch/studio/script.js
@@ -1,4 +1,6 @@
 //TODO: Add Your Code Below
 window.onload = function() {
-    
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response) {
+        return response;
+    });
 }
